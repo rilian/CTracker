c1d859c43573c1d9614407163fede95bfc976d6b
rilian <dmitriyis@gmail.com>
2013-07-28 22:57:39 +0300

    prettify models

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,22 +1,43 @@
 class Currency < ActiveRecord::Base
-  set_primary_key :code
-  attr_accessible :name, :code, :country_id
+  # Includes
 
+  # Before, after callbacks
+
+  # Default scopes, default values (e.g. self.per_page =)
+
+  # Associations: belongs_to > has_one > has_many > has_and_belongs_to_many
+  belongs_to :country
+
+  # Validations: presence > by type > validates
   validates_presence_of :name
   validates_presence_of :code
   validates_uniqueness_of :code, :allow_blank => true
 
-  belongs_to :country
+  # Other properties (e.g. accepts_nested_attributes_for)
+  self.primary_key = 'code'
 
+  attr_accessible :name, :code, :country_id
+
+  # Model dictionaries, state machine
+
+  # Scopes
+  class << self
+  end
+
+  # Other model methods
   def self.collected
-    all.select {|currency| currency.collected? }
+    all.select { |currency| currency.collected? }
   end
 
   def self.not_collected
-    all.reject {|currency| currency.collected? }
+    all.reject { |currency| currency.collected? }
   end
 
   def collected?
     country.nil? ? false : country.visited?
   end
+
+  # Private methods (for example: custom validators)
+  private
+
 end
