3c2cdac41d7b385ef0981d4292f9a1b2756b9b16
rilian <dmitriyis@gmail.com>
2013-07-29 18:31:09 +0300

    moving tests around

--- test/models/data_updater_test.rb
+++ test/models/data_updater_test.rb
@@ -4,7 +4,7 @@ class DataUpdaterTest < ActiveSupport::TestCase
   test "parse_response returns correct hash structure" do
     response = { :get_currencies_response => { :get_currencies_result => "<NewDataSet>\n  <Table>\n    <Name>Afghanistan, Islamic State of</Name>\n    <CountryCode>af</CountryCode>\n    <Currency>Afghani</Currency>\n    <CurrencyCode>AFA</CurrencyCode>\n  </Table>\n  </NewDataSet>" } }
     expected = {
-      :countries  => [{ :name => "Afghanistan, Islamic State of", :code => "af"   }],
+      :countries  => [{ :name => "Afghanistan, Islamic State of", :code => "af" }],
       :currencies => [{ :name => "Afghani", :code => "AFA", :country_id => "af" }]
     }
 
