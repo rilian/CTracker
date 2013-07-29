4064abda265a95c7908b75c560268f46cb5d5159
rilian <dmitriyis@gmail.com>
2013-07-29 18:32:19 +0300

    refactoring collected currency by a current user

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -7,6 +7,7 @@ class Currency < ActiveRecord::Base
 
   # Associations: belongs_to > has_one > has_many > has_and_belongs_to_many
   belongs_to :country
+  has_many :user_countries, through: :country#, source: :currency
 
   # Validations: presence > by type > validates
   validates_presence_of :name
@@ -24,17 +25,23 @@ class Currency < ActiveRecord::Base
   class << self
   end
 
+  scope :collected_by_user, ->(user) {
+    user_id = UserCountry.arel_table[:user_id]
+
+    joins(:country).joins(:user_countries).where(user_id.eq(user.id))
+  }
+
+  scope :not_collected_by_user, ->(user) {
+    user_countries = UserCountry.select('country_id').where(user_id: user.id).to_sql
+    country_id = self.arel_table[:country_id]
+
+    where(country_id.not_in(user_countries))
+  }
+
   # Other model methods
-  def self.collected
-    all.select { |currency| currency.collected? }
-  end
 
-  def self.not_collected
-    all.reject { |currency| currency.collected? }
-  end
-
-  def collected?
-    country.nil? ? false : country.visited?
+  def collected_by_user?(user)
+    country.nil? ? false : user_countries.of_user(user).exists?
   end
 
   # Private methods (for example: custom validators)
