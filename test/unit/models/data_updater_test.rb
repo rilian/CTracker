a2cbc6cc2db0257c9a468bf62bd10150da7b3e33
rilian <dmitriyis@gmail.com>
2013-07-30 21:28:58 +0300

    fix fixture in data_updater test

--- test/unit/models/data_updater_test.rb
+++ test/unit/models/data_updater_test.rb
@@ -4,8 +4,8 @@ class DataUpdaterTest < ActiveSupport::TestCase
   test "parse_response returns correct hash structure" do
     response = { :get_currencies_response => { :get_currencies_result => "<NewDataSet>\n  <Table>\n    <Name>Afghanistan, Islamic State of</Name>\n    <CountryCode>af</CountryCode>\n    <Currency>Afghani</Currency>\n    <CurrencyCode>AFA</CurrencyCode>\n  </Table>\n  </NewDataSet>" } }
     expected = {
-      :countries  => [{ :name => "Afghanistan, Islamic State of", :code => "af" }],
-      :currencies => [{ :name => "Afghani", :code => "AFA", :country_id => "af" }]
+      :countries =>  [{:name => "Afghanistan, Islamic State of", :code => "af"}],
+      :currencies => [{:name => "Afghani", :code => "AFA", :country_code => "af"}]
     }
 
     DataUpdater.send(:public, :parse_response)
